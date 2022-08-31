2d9a5d97237a4d6be4016136572a8c6fdb1e77ce
hristo <hristo.georgiev@telepol.com>
2022-08-31 14:39:08 +0300

    change data

---
+++ data/monuments.js
@@ -0,0 +1,74 @@
+let monuments = [
+	{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Бузлуджа",
+		"title_second" : "Паметник на Бузлуджа",
+		"info": "Бузлуджа информация",
+		"img_path": "img/my_img/monuments/buzludza/buzludza_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+		{
+		"title": "Шипка",
+		"title_second" : "Паметник на свободата",
+		"info": "Шинка информация",
+		"img_path": "img/my_img/monuments/shipka/shipka_1.jpg"
+	},
+]
